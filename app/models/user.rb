e461f021030c2066c5d0c50fc5bb12501760ef00
marielfrank <marielbeth@gmail.com>
2018-03-21 17:24:51 -0400

    Set up user model, add admin property to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,36 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    # use bcrypt for password security
+    has_secure_password
+
+    has_many :puzzles
+    has_many :reviews
+    belongs_to :location
+
+    # authentication/authorization methods
+    def admin?
+        self.admin
+    end
+
+    # # check if user is admin or owner of a resource
+    # def owner_or_admin?(resource)
+    #     if admin?
+    #         true
+    #     elsif resource.respond_to?('user')
+    #         resource.user == self
+    #     else
+    #         false
+    #     end
+    # end
+
+    # find or create by oauth user_id
+    # def self.set_user_from_oauth(auth)
+    #     find_or_create_by(uid: auth['uid']) do |u|
+    #         u.name = auth['info']['name']
+    #         u.email = auth['info']['email']
+    #         # set random secure password if new user
+    #         u.password ||= SecureRandom.base58
+    #     end
+    # end
 end
