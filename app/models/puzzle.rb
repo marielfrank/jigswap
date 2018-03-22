f04b8ce87d60c6cf9b32a770faa1ebfb7eba5187
marielfrank <marielbeth@gmail.com>
2018-03-22 12:34:01 -0400

    Set up puzzle.rb with validations, object relationships, & #change_owner

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,2 +1,17 @@
 class Puzzle < ApplicationRecord
+    validates :name, uniqueness: true
+    validates :pieces, presence: true, numericality: true { only_integer: true }
+
+    belongs_to :user
+    has_many :puzzle_tags
+    has_many :tags, through: :puzzle_tags
+    has_many :reviews
+    delegate :location, to :user 
+
+    def change_owner(owner_id)
+        prev_owner = self.user
+        self.previous_owners.push(prev_owner)
+        new_owner = User.find(owner_id)
+        self.user = new_owner
+    end
 end
