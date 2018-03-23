593b1275ea38ebd6c32be31177c548511b3fbbda
marielfrank <marielbeth@gmail.com>
2018-03-23 09:45:37 -0400

    Fix previous_owners data type with new migration

---
+++ db/migrate/20180323124330_change_previous_owners_to_integer_array_in_puzzles.rb
@@ -0,0 +1,6 @@
+class ChangePreviousOwnersToIntegerArrayInPuzzles < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :puzzles, :previous_owners, :string, array: true
+    add_column :puzzles, :previous_owners, :integer, array: true
+  end
+end
