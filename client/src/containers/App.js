fc3bfada78de188cf65939ea4122eb9c81e9c181
marielfrank <marielbeth@gmail.com>
2018-03-27 15:32:03 -0400

    Add components & containers folders, add app.js to containers

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {fetchPuzzles} from './actions/puzzleActions'
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 
 class App extends Component {
   componentDidMount() {
@@ -16,8 +16,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-          {console.log(this.props.puzzleState)}
-          {this.props.puzzleState.puzzles.map(puz => <div key={puz.id}><h3>{puz.name}</h3><p>{puz.pieces}</p></div>)}
+          {this.props.puzzleState.puzzles.map(puz => 
+            <div key={puz.id}><h3>{puz.name}</h3><p>{puz.pieces}</p></div>
+          )}
       </div>
     );
   }
