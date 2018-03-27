b7342949d316b5d68685cd7b1f3eec2370829286
marielfrank <marielbeth@gmail.com>
2018-03-27 11:04:06 -0400

    Fix CORS issues in gemfile, config/cors.rb, puzzleActions

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Use dotenv for passwords
 gem 'dotenv-rails', groups: [:development, :test]
 
+# Add CORS headers
+gem 'rack-cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- client/src/actions/puzzleActions.js
+++ client/src/actions/puzzleActions.js
@@ -3,7 +3,7 @@ import fetch from 'isomorphic-fetch';
 export function fetchPuzzles() {
     return (dispatch) => {
         dispatch({type: 'LOADING_PUZZLES'})
-        return fetch('http://localhost:3000/puzzles.json')
+        return fetch('http://localhost:3001/puzzles.json')
         .then(response => response.json())
         .then(puzzles => dispatch({ type: 'FETCH_PUZZLES', payload: puzzles }));
     }
