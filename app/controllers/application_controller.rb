d20f45c47a2218f9551660b9aff11eddfaac98ab
marielfrank <marielbeth@gmail.com>
2018-03-28 17:16:09 -0400

    Add knock gem, install in routes.rb, include in app & user controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,10 @@
 class ApplicationController < ActionController::API
+    include Knock::Authenticable
+
     private
 
     def logged_in?
         !!current_user
-    end
-
-    def current_user
-        User.find_by(id: session[:user_id])
-    end
-
-    def log_user_in
-        session[:user_id] = @user.id
-        # do something?
     end
 
 end
