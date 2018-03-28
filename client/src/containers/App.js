d974d67048cc67f77e19a70555682716a31e7133
marielfrank <marielbeth@gmail.com>
2018-03-28 11:10:21 -0400

    Fix app.js file relationships

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {fetchPuzzles} from './actions/puzzleActions'
-import logo from './logo.svg';
+import {fetchPuzzles} from '../actions/puzzleActions'
+import logo from '../logo.svg';
 import '../App.css';
 
 class App extends Component {
