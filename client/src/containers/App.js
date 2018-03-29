ed4d904e06f633e7aa900f51a4f3125a12235fa4
marielfrank <marielbeth@gmail.com>
2018-03-29 12:23:04 -0400

    Add form, #handleOnChange, #handleOnSubmit to app.js, add useractions.js

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {fetchPuzzles} from '../actions/puzzleActions';
+import {createUser} from '../actions/userActions';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import logo from '../logo.svg';
 import '../App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      username: '',
+      email: '', 
+      password: ''
+    };
+  }
+
   componentDidMount() {
     this.props.fetchPuzzles();
+  }
+
+  handleOnChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(e)
+    // this.props.createUser(this.state);
   }
 
   render() {
@@ -23,13 +48,25 @@ class App extends Component {
           {this.props.puzzleState.puzzles.map(puz => 
             <div key={puz.id}><h3>{puz.name}</h3><p>{puz.pieces}</p></div>
           )}
-          <form>
+          <form onSubmit={this.handleOnSubmit}>
+            <label htmlFor="email">Username: </label>
+            <br />
+            <input
+              name="username"
+              id="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleOnChange}
+            />
+            <br /><br />
             <label htmlFor="email">Email: </label>
             <br />
             <input
               name="email"
               id="email"
               type="email"
+              value={this.state.email}
+              onChange={this.handleOnChange}
             />
             <br /><br />
             <label htmlFor="password">Password:</label>
@@ -38,14 +75,15 @@ class App extends Component {
               name="password"
               id="password"
               type="password"
+              value={this.state.password}
+              onChange={this.handleOnChange}
+            />
+            <br /><br />
+            <input
+              type="submit"
+              value="Create Account"
             />
           </form>
-          <br />
-          <button
-            onClick={this.login}
-          >
-              Login
-          </button>
         <br />
 
         <Footer/>
@@ -60,4 +98,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default App = connect(mapStateToProps, {fetchPuzzles})(App);
+export default App = connect(mapStateToProps, {fetchPuzzles, createUser})(App);
