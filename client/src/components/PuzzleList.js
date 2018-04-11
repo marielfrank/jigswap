7df3651a90de528feed2407186c404b7070c81a5
marielfrank <marielbeth@gmail.com>
2018-04-11 11:20:50 -0400

    Fix routes in app to incude props, add user info to account.js

--- client/src/components/PuzzleList.js
+++ client/src/components/PuzzleList.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const PuzzleList = ({match, puzzles}) => {
-    return (<div className="grid-container" >{puzzles.map(puz => 
-        <Link className="puzzle-list-item grid-item" key={puz.id} to={`/puzzles/${puz.id}`}>
-            {puz.name} - {puz.pieces} pieces
-        </Link>
-    )}</div>)
+    return (
+        <div className="grid-container" >
+            {puzzles.map(puz => 
+                <Link className="puzzle-list-item grid-item" key={puz.id} to={`/puzzles/${puz.id}`}>
+                    {puz.name} - {puz.pieces} pieces
+                </Link>
+            )}
+        </div>
+    )
 }
 
 export default PuzzleList;
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import PuzzleList from './PuzzleList';
 
-const User = () => (
-    <div id="user">
-        User component stuff
-    </div>
-)
-
+const User = ({user}) => {
+    console.log(user)
+    return (
+        <div id="user">
+        <h2>Your account</h2>
+        <p><strong>Username:</strong> {user.username}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        <p><strong>Puzzles you're sharing:</strong></p>
+        <PuzzleList puzzles={user.puzzles} />
+        </div>
+    )
+}
 export default User;
